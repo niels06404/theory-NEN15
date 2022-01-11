d81571255b18b1169c6320ebe7be2f19dee3dfce
Niels Schroder <nielsschroder1234@gmail.com>
2022-01-11 17:00:55 +0100

    Added list/dict comprehension to load functions

--- load.py
+++ load.py
@@ -1,29 +1,33 @@
 from station import Station
-import csv
+
 
 def load_stations(input_file):
-    stations = {}
-    with open(input_file) as file:
-        reader = csv.reader(file)
-        next(reader)
-        while True:
-            line = file.readline()
-            if line == "":
-                break
-            split = line.split(",")
-            stations[split[0]] = Station((split[0]), float(split[1]), float(split[2]))
+    with open(input_file, 'r') as file:
+        # Skip header line
+        next(file)
+        
+        stationlist = []
+        
+        for line in file:
+            stationlist.append(line.strip().split(","))
+            
+        # Add station information to dictionary        
+        stations = {x[0]: Station(x[0], float(x[1]), float(x[2])) for x in stationlist}
+
     return stations
 
+
 def load_connections(input_file, stations):
-    with open(input_file) as file:
-        reader = csv.reader(file)
-        next(reader)
-        while True:
-            line = file.readline()
-            if line == "":
-                break
-            split = line.split(",")
+    with open(input_file, 'r') as file:
+        # Skip header line
+        next(file)
+        
+        connectionlist = []
+        
+        for line in file:
+            connectionlist.append(line.strip().split(","))
             
-            # Twee richtingen
-            stations[split[0]].add_connection(split[1], int(split[2]))
-            stations[split[1]].add_connection(split[0], int(split[2]))
+        # Add connections to stations            
+        for x in connectionlist:
+            stations[x[0]].add_connection(x[1], int(x[2]))
+            stations[x[1]].add_connection(x[0], int(x[2]))
