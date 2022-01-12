1e6ba394549dee378f2c1e1ecf4060c5425cf272
Niels Schroder <nielsschroder1234@gmail.com>
2022-01-12 11:03:48 +0100

    Defined main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from load import load_stations, load_connections
 
+def main():
+    load_mode = 'Holland'
+    
+    stations = load_stations(f'data/Stations{load_mode}.csv')
+    load_connections(f'data/Connecties{load_mode}.csv', stations)
 
-stations = load_stations("data/StationsHolland.csv")
-load_connections("data/ConnectiesHolland.csv", stations)
+    # Test
+    print(stations)
+    print(stations["Rotterdam Centraal"]._connections)
 
-# Test
-print(stations)
-print(stations["Rotterdam Centraal"]._connections)
 
+if __name__ == '__main__':
+    main()
