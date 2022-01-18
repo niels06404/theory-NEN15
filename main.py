77f45539f577e51858f0b76904db92683422ba07
Niels Schroder <nielsschroder1234@gmail.com>
2022-01-18 22:11:15 +0100

    Num of runs must now be provided on command line

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from code.classes.stations_graph import StationsGraph
 
 import pandas as pd
 
-from visualization import cleanup_connections, visualization
+# from visualization import cleanup_connections, visualization
 
 
-def main(the_map, output_file):
-    RUNS = 10000
+def main(the_map, output_file, RUNS):
     print(f"Calculating best routes out of {RUNS} runs...")
 
     # Load data into the graph
@@ -33,25 +32,29 @@ def main(the_map, output_file):
     print(f"See '{output_file}' for generated routes.")
 
     # Output info to file for personal use
+    generate_personal_output(best_graph, average_score, RUNS)
+
+    print("Loading visualization...")
+    # visualization(the_map, best_graph)
+    print(f"Done! See 'test_{the_map}.png' for visualization.")
+
+
+def generate_personal_output(graph, average_score, RUNS):
     sys.stdout = open("output.txt", "w")
-    for i, route in enumerate(best_graph.routes.values()):
+    for i, route in enumerate(graph.routes.values()):
         print(f"Route {i + 1}", end=': ')
         for station in route.stations:
             print(station._name, end=', ')
         print("Time:", route.time)
     print()
-    print("Unvisited connections:", cleanup_connections(best_graph.get_unused_connections()), ", Length:", len(cleanup_connections(best_graph.get_unused_connections())))
-    print("p =", len(best_graph.get_visited_connections()) / len(best_graph.connections))
-    print("T =", len(best_graph.routes))
-    print("Min =", sum([route.time for route in best_graph.routes.values()]))
-    print("Score:", best_graph.calculate_score())
+    # print("Unvisited connections:", cleanup_connections(graph.get_unused_connections()), ", Length:", len(cleanup_connections(graph.get_unused_connections())))
+    print("p =", len(graph.get_visited_connections()) / len(graph.connections))
+    print("T =", len(graph.routes))
+    print("Min =", sum([route.time for route in graph.routes.values()]))
+    print("Score:", graph.calculate_score())
     print(f"Average score: {average_score} out of {RUNS} runs")
     sys.stdout.close()
     sys.stdout = sys.__stdout__
-
-    print("Loading visualization...")
-    visualization(the_map, best_graph)
-    print(f"Done! See 'test_{the_map}.png' for visualization.")
 
 
 def generate_output(graph, output_file):
@@ -74,9 +77,10 @@ if __name__ == "__main__":
     # Adding arguments
     parser.add_argument("the_map", choices=["Nationaal", "Holland"])
     parser.add_argument("output_file", help="output file (csv)")
+    parser.add_argument("N", help="Number of runs", type=int)
 
     # Read arguments from command line
     args = parser.parse_args()
 
     # Run main with provided arguments
-    main(args.the_map, args.output_file)
+    main(args.the_map, args.output_file, args.N)
