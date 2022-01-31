ecb4a57ab143799835ae8730edb7fc1e50ab94d6
Niels Schroder <nielsschroder1234@gmail.com>
2022-01-31 18:04:34 +0100

    Converted randomizer to Class

--- code/algorithms/randomize.py
+++ code/algorithms/randomize.py
@@ -3,56 +3,69 @@ import pickle
 import random
 
 
-def random_assignment(input_graph, the_map):
-    '''
-    Randomly assigns valid stations to routes.
-    '''
-    # graph = copy.deepcopy(input_graph)
-    graph = pickle.loads(pickle.dumps(input_graph))
+class Random:
+    def __init__(self, input_graph, the_map):
+        # self.graph = copy.deepcopy(input_graph)
+        self.graph = pickle.loads(pickle.dumps(input_graph))
+        self.map = the_map
+        self.MAX_LENGTH = 20 if the_map == "Nationaal" else 7
+        self.MAX_TIME = 180 if the_map == "Nationaal" else 120
+    
+    def run(self):
+        '''
+        Executes the algorithm.
+        '''
+        unavailable_options = set()
+        while not len(self.graph.routes) >= self.MAX_LENGTH and len(self.graph.get_visited_connections()) < len(self.graph.connections):
+            starting_station = self.get_starting_station()
 
-    unavailable_options = set()
-    if the_map == "Nationaal":
-        MAX_LENGTH = 20
-        MAX_TIME = 180
-    else:
-        MAX_LENGTH = 7
-        MAX_TIME = 120
+            # Create a new route with starting station
+            self.graph.add_route(self.graph.stations[starting_station])
 
-    # Keep adding routes until max amount is reached or all stations are connected
-    while not len(graph.routes) >= MAX_LENGTH and len(graph.get_visited_connections()) < len(graph.connections):
-        # First element can pick any station
-        starting_station = random.choice(list(graph.stations))
+            # Keep adding stations to route until time exceeds limit
+            key = list(self.graph.routes.keys())[-1]
+            route = self.graph.routes[key]
 
-        # Create a new route with starting station
-        graph.add_route(graph.stations[starting_station])
+            while route.time < self.MAX_TIME:
+                # Stop algorithm from visiting the same station twice in one route
+                for station in route.stations:
+                    unavailable_options.add(station._name)
 
-        # Keep adding stations to route until time exceeds limit
-        key = list(graph.routes.keys())[-1]
-        route = graph.routes[key]
+                possibilities = route.get_possibilities(unavailable_options)
 
-        while route.time < MAX_TIME:
-            # Stop algorithm from visiting the same station twice in one route
-            for station in route.stations:
-                unavailable_options.add(station._name)
+                # Stop adding if no options are left
+                if len(possibilities) == 0:
+                    unavailable_options = set()
+                    break
 
-            possibilities = route.get_possibilities(unavailable_options)
+                # Otherwise randomly pick a station
+                choice = random.choice(possibilities)
 
-            # Stop adding if no options are left
-            if len(possibilities) == 0:
+                # Add randomly picked station to route
+                route.add_station(self.graph.stations[choice], route.stations[-1]._connections[choice])
                 unavailable_options = set()
-                break
 
-            # Otherwise randomly pick a station
-            choice = random.choice(possibilities)
+            # Remove route if it is not valid
+            if not route.is_valid(self.map):
+                del self.graph.routes[key]
+            elif route.is_valid(self.map):
+                self.graph.count_visited_connections(route.connections)
+    
+    def get_starting_station(self):
+        '''
+        Returns a random starting station
+        '''
+        return random.choice(list(self.graph.stations))
 
-            # Add randomly picked station to route
-            route.add_station(graph.stations[choice], route.stations[-1]._connections[choice])
-            unavailable_options = set()
 
-        # Remove route if it is not valid
-        if not route.is_valid(the_map):
-            del graph.routes[key]
-        elif route.is_valid(the_map):
-            graph.count_visited_connections(route.connections)
-
-    return graph
+class NewRandom(Random):
+    def get_starting_station(self):
+        '''
+        Returns a station from a connection with the least number of connections made.
+        '''
+        possibilities = [connection for connection in self.graph.connections if self.graph.connections[connection] == 0]
+        possibility = random.choice(possibilities)
+        while self.graph.connections[(possibility[1], possibility[0])] != 0:
+            possibility = random.choice(possibilities)
+        
+        return random.choice(possibility)
